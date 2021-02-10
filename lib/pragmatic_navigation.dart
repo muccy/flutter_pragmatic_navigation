ae35b89a7cf4bd72782544d76ef7a5935fdd179e
Muccy <muccymac@gmail.com>
2021-02-10 16:49:05 +0100

    Stack data structure

--- lib/pragmatic_navigation.dart
+++ lib/pragmatic_navigation.dart
@@ -1,7 +1,3 @@
 library pragmatic_navigation;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'stack.dart';
---
+++ lib/stack.dart
@@ -0,0 +1,37 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+
+/// Data structure that represents the navigation stack. It notifies changes made to [items] list, even through [push]
+/// and [pop] operations.
+class NavigationStack<T> with ChangeNotifier {
+  List<T> _items;
+  NavigationStack(List<T> items) : _items = List.of(items) ?? [];
+
+  /// Returns an immutable copy of items in this stack
+  UnmodifiableListView<T> get items => UnmodifiableListView(_items);
+
+  /// Replaces [items] with a copy of given [newItems] and notifies changes
+  set items(List<T> newItems) {
+    _items = List.from(newItems ?? []);
+    notifyListeners();
+  }
+
+  /// Push a new [item] at the end of the stack and notifies change
+  void push(T item) {
+    _items.add(item);
+    notifyListeners();
+  }
+
+  /// Removes last item is possible and notifies change.
+  /// It returns the popped item, or null if stack was empty.
+  T pop() {
+    try {
+      final poppedItem = _items.removeLast();
+      notifyListeners();
+      return poppedItem;
+    } catch (e) {
+      return null;
+    }
+  }
+}
